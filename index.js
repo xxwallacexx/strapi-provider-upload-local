5a93c1be51377ff8dddc2afa6187dfa9753e6a8c
wallacewong <wallacepuit@gmail.com>
2025-02-14 12:10:47 +0000

    fix: remove dateformat

--- index.js
+++ index.js
@@ -19,22 +19,22 @@ module.exports = {
       UPLOADS_FOLDER_NAME
     );
 
-    const formatDate = () => {
-      const date = new Date();
-      const year = date.getFullYear();
-      const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-indexed
-      const day = String(date.getDate()).padStart(2, "0");
-
-      return `${year}-${month}-${day}`;
-    };
+    // const formatDate = () => {
+    //   const date = new Date();
+    //   const year = date.getFullYear();
+    //   const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-indexed
+    //   const day = String(date.getDate()).padStart(2, "0");
+    //
+    //   return `${year}-${month}-${day}`;
+    // };
 
     return {
       upload(file) {
         return new Promise((resolve, reject) => {
           // write file in public/assets folder
           const folder = file.path
-            ? `/${formatDate()}/${file.path}`
-            : `/${formatDate()}`;
+            ? `/${file.path}`
+            : `/`;
 
           const dir = path.join(uploadDir, folder);
 
